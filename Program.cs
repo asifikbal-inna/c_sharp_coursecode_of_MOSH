64c95d8ea78b7fb3a38ce8d79878a22779bba0c2
Md Asif Ikbal <75169639+asifikbal-inna@users.noreply.github.com>
2023-05-07 10:17:52 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace while_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i<=10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
---
+++ person.cs
@@ -0,0 +1,13 @@
+﻿namespace classes
+{
+    public class person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void introduce()
+        {
+            Console.WriteLine("My Name is :" +" "+FirstName+" "+LastName);
+        }
+    }
+}
